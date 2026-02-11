6affc5215b6dfdb709d250ffd3fcbe9bdd299fdc
tonitechsmps-cpu <tonitechsmps@gmail.com>
2026-02-11 11:50:40 +0530

    Replace HashRouter with createRoot for rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import { HashRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 );
