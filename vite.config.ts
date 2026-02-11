002d92a04dda26e890782447b254a7989c6a0bbb
tonitechsmps-cpu <tonitechsmps@gmail.com>
2026-02-11 10:41:29 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',   // 👈 YE LINE ADD KARO
+
   server: {
     host: "::",
     port: 8080,
